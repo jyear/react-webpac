1ff294e84798f1548bec0747652e2693c3e7d9d8
杨叙 <yx53074@hotmail.com>
2018-07-06 17:54:27 +0800

    22

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const webpackBaseConfig = require("./build/webpack.base");
-const webpackDevConfig = require("./build/webpack.dev");
-const webpackProConfig = require("./build/webpack.pro");
 const webpackMerge = require("webpack-merge");
 var webpackConfig = null;
 if (process.env.NODE_ENV === "development") {
+	const webpackDevConfig = require("./build/webpack.dev");
 	webpackConfig = webpackMerge(webpackBaseConfig, webpackDevConfig);
 }
 if (process.env.NODE_ENV === "production") {
+	const webpackProConfig = require("./build/webpack.pro");
 	webpackConfig = webpackMerge(webpackBaseConfig, webpackProConfig);
 }
 module.exports = webpackConfig;
